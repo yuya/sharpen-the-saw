a7d424637c34ea289f7ff8ecaf268dd146b86845
yuya <twilightwind@gmail.com>
2014-05-16 00:20:04 +0900

    p-15

---
+++ the-c-programming-language/p-15/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* fahr = 0, 20, ..., 300 に対して、
+ * 摂氏 - 華氏対応表を作成する */
+int main() {
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;      /* 温度表の上限 */
+    upper = 300;    /* 上限 */
+    step  = 20;     /* きざみ */
+    fahr  = lower;
+
+    while (fahr <= upper) {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
+
